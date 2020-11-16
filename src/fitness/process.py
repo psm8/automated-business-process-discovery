393fff14176c895cea40a5f17428948f5a7a94c0
psm8 <43780167+psm8@users.noreply.github.com>
2020-11-16 19:22:36 +0100

    metrics in progress

--- src/fitness/process.py
+++ src/fitness/process.py
@@ -1,6 +1,21 @@
 from fitness.base_ff_classes.base_ff import base_ff
 from gate.gate import Gate
 
+import math
+
+
+def string_to_dictionary(string: str):
+    dictionary = dict()
+    i = 0
+    for x in string:
+        dictionary[i] = (x, -1)
+        i += 1
+    return dictionary
+
+
+def get_model_average_length():
+    return 3.5
+
 
 def calculate_simplicity_metric(s):
     return
@@ -10,7 +25,7 @@ def calculate_fitness_metric(gate):
     list1 = ["abcd", "abd", "acd", "abdc"]
     result = 0
     for elem in list1:
-        result += gate.traverse(elem)
+        result += gate.traverse(elem, len(elem))
 
     return result/14
 
@@ -28,6 +43,10 @@ def calculate_length_metric(guess, goal_length):
     return fitness
 
 
+def calculate_max_allowed_length():
+    return math.floor(2 * get_model_average_length()) - 1
+
+
 class process(base_ff):
     maximise = True
 
@@ -40,7 +59,12 @@ class process(base_ff):
 
         gate = Gate("seq")
         gate.parse(guess)
-
+        length = gate.get_model_minimal_length()
+        if length > calculate_max_allowed_length():
+            return -100000
+        processes = gate.get_processes_list()
+        test = string_to_dictionary("abcd")
+        first_occurrence = gate.find_first_occurrence(string_to_dictionary("abcd"), 0)
         length_metric = calculate_length_metric(guess, 50)
         fitness_metric = calculate_fitness_metric(gate)
         return length_metric * fitness_metric
--- src/gate/gate.py
+++ src/gate/gate.py
@@ -1,4 +1,6 @@
-from itertools import islice
+import math
+
+from itertools import islice, permutations
 import collections
 
 
@@ -53,69 +55,249 @@ class Gate:
     #             if elem == process:
     #                 raise Exception
 
-    def traverse(self, expression: str) -> int:
-        matches = 0
+    def traverse(self, expression: str, goal_length: int) -> int:
+        cumulative_error = 0
+        matches = []
+        if self.name == "and":
+            return 0
+        elif self.name == "xor":
+            return 0
+        elif self.name == "seq":
+            return sum(self.traverse_process_children(expression))
+        # python way to check empty
+        elif self.name == "opt":
+            return 0
+        elif self.name == "trm":
+            return 0
+        elif self.name == "lop":
+            return 0
+        else:
+            raise Exception
+
+    # def traverse_2(self, expression: str, goal_length: int) -> int:
+    #     cumulative_error = 0
+    #     matches = []
+    #     if self.name == "and":
+    #         if not elem in matches
+    #             cumulative_error +=1
+    #         else:
+    #             next elem
+    #     elif self.name == "xor":
+    #         if not elem in matches
+    #             cumulative_error += 1
+    #     elif self.name == "seq":
+    #         if elem != matches[0]:
+    #             cumulative_error += 1
+    #     # python way to check empty
+    #     elif self.name == "opt":
+    #         if elem in matches:
+    #             next elem
+    #     elif self.name == "trm":
+    #         return 0
+    #     elif self.name == "lop":
+    #         if elem == matches[0]:
+    #             next elem
+    #     else:
+    #         raise Exception
+
+    def find_first_occurrence(self, global_processes: dict, flag: int) -> []:
+        all_processes = []
+        elements = self.elements.copy()
+
+        while flag < len(global_processes) or global_processes.get(flag)[1] - flag <= len(global_processes) / 2:
+            # python way to check empty
+            if elements:
+                if self.name == "and":
+                    elem = elements.pop(0)
+                    # python way to check empty
+                    while elements:
+                        processes = global_processes.copy()
+                        if isinstance(elem, str):
+                            for i in range(flag, len(processes)):
+                                processes[i] = (processes.get(i)[0], processes.get(i)[1] + 1)
+                                if elem == processes.get(flag)[0]:
+                                    flag += 1
+                                # the difference between and and xor
+                                self.find_first_occurrence(processes, flag)
+                            all_processes.append(processes)
+                        else:
+                            all_processes.append(elem.find_first_occurrence(processes, flag))
+                elif self.name == "xor":
+                    elem = elements.pop(0)
+                    # python way to check empty
+                    while elements:
+                        processes = global_processes.copy()
+                        if isinstance(elem, str):
+                            for i in range(flag, len(processes)):
+                                processes[i] = (processes.get(i)[0], processes.get(i)[1] + 1)
+                                if elem == processes.get(flag)[0]:
+                                    flag += 1
+                            all_processes.append(processes)
+                        else:
+                            all_processes.append(elem.find_first_occurrence(processes, flag))
+                elif self.name == "seq":
+                    elem = elements.pop(0)
+                    if isinstance(elem, str):
+                        if elem == global_processes.get(flag)[0]:
+                            for i in range(flag, len(global_processes)):
+                                global_processes[i] = (global_processes.get(i)[0],
+                                                       global_processes.get(i)[1] + 1)
+                    else:
+                        global_processes = elem.find_first_occurrence(global_processes, flag)
+                    flag += 1
+                elif self.name == "opt":
+                    elem = elements.pop(0)
+                    # python way to check empty
+                    while elements:
+                        processes = global_processes.copy()
+                        if isinstance(elem, str):
+                            for i in range(flag, len(processes)):
+                                processes[i] = (processes.get(i)[0], processes.get(i)[1] + 1)
+                                if elem == processes.get(flag)[0]:
+                                    flag += 1
+                                # the difference between and and xor
+                                self.find_first_occurrence(processes, flag)
+                            all_processes.append(processes)
+                        else:
+                            all_processes.append(elem.find_first_occurrence(processes, flag))
+                    flag += 1
+                elif self.name == "trm":
+                    return global_processes
+                elif self.name == "lop":
+                    elem = elements.pop(0)
+                    if isinstance(elem, str):
+                        if elem == global_processes.get(flag)[0]:
+                            for i in range(flag, len(global_processes)):
+                                global_processes[i] = (global_processes.get(i)[0],
+                                                       global_processes.get(i)[1] + 1)
+                                flag += 1
+                    else:
+                        global_processes = elem.find_first_occurrence(global_processes, flag)
+                else:
+                    raise Exception
+
+            else:
+                for i in range(flag, len(global_processes)):
+                    global_processes[i] = (global_processes.get(i)[0], -1)
+                return global_processes
+
+        for i in range(flag, len(global_processes)):
+            global_processes[i] = (global_processes.get(i)[0], -1)
+        return global_processes
+
+    def remove_policy_and(self, all_processes: []):
+        new_global_processes = []
+
+        return new_global_processes
+
+    def remove_policy_xor(self, all_processes: []) -> []:
+        results = dict()
+        for processes in all_processes:
+            for i in range(len(processes)):
+                if processes == -1:
+                    if i > 0:
+                        results[processes] = (processes.get(i)[1] + 1)/i
+                    else:
+                        results[processes] = 999
+
+        new_all_processes = [k for k, v in results.items() if float(v) >= min(results, key=results.get)]
+
+        return new_all_processes
+
+    def continue_processing_xor(self, all_processes):
+        new_global_processes = []
+
+        return new_global_processes
+
+    def traverse_and_gate(self, expression: str, goal_length: int) -> []:
+        if self.traverse_inner() == goal_length:
+            return list(permutations([1, 2, 3]))
+
+    def traverse_inner(self) -> []:
+        processes = []
+        iterator = 0
+        elements = self.elements.copy()
+
+        while elements:
+            elem = elements.pop(0)
+            if isinstance(elem, str):
+                processes.append(elem)
+            else:
+                processes.append(elem.traverse())
+                iterator += 1
+
+        return processes
+
+    def get_model_minimal_length(self) -> int:
+        if self.name == "and":
+            length = sum(self.get_model_minimal_length_process_children())
+        elif self.name == "xor":
+            length = min(self.get_model_minimal_length_process_children())
+        elif self.name == "seq":
+            length = sum(self.get_model_minimal_length_process_children())
+        elif self.name == "opt":
+            length = 0
+        elif self.name == "trm":
+            length = 0
+        elif self.name == "lop":
+            length = 0
+        else:
+            raise Exception
+        return length
+
+    def get_model_minimal_length_process_children(self) -> []:
+        lengths = []
+        elements = self.elements.copy()
+
+        while elements:
+            elem = elements.pop(0)
+            if isinstance(elem, str):
+                lengths.append(1)
+            else:
+                lengths.append(elem.get_model_minimal_length())
+
+        return lengths
+
+    def traverse_process_children(self, expression: str) -> []:
+        matches = []
+        iterator = 0
         elements = self.elements.copy()
 
         while elements:
             if not expression:
-                return matches
+                break
             else:
                 elem = elements.pop(0)
                 if isinstance(elem, str):
                     if expression[0] == elem:
-                        matches += 1
+                        if len(matches) <= iterator:
+                            matches.append(1)
+                            iterator += 1
+                        else:
+                            matches[iterator] += 1
                         expression = expression[1:]
+
                 else:
-                    if self.name == "and":
-                        return matches
-                    elif self.name == "xor":
-                        return matches
-                    elif self.name == "seq":
-                        matches = elem.traverse(expression)
-                        return matches
-                    # python way to check empty
-                    elif self.name == "opt":
-                        return matches
-                    elif self.name == "trm":
-                        return matches
-                    elif self.name == "lop":
-                        return matches
-                    else:
-                        raise Exception
+                    matches.append(elem.traverse(expression, len(expression)))
+                    iterator += 1
+
         return matches
 
-    def get_model_minimal_length(self):
-        length = 0
+    def get_processes_list(self) -> []:
+        processes = []
+        iterator = 0
         elements = self.elements.copy()
 
         while elements:
-            if not expression:
-                return length
+            elem = elements.pop(0)
+            if isinstance(elem, str):
+                processes.append(elem)
             else:
-                elem = elements.pop(0)
-                if isinstance(elem, str):
-                    if expression[0] == elem:
-                        length += 1
-                        expression = expression[1:]
-                else:
-                    if self.name == "and":
-                        return length
-                    elif self.name == "xor":
-                        return length
-                    elif self.name == "seq":
-                        length = elem.get_model_minimal_length(expression)
-                        return length
-                    # python way to check empty
-                    elif self.name == "opt":
-                        return length
-                    elif self.name == "trm":
-                        return length
-                    elif self.name == "lop":
-                        return length
-                    else:
-                        raise Exception
-        return matches
+                processes.append(elem.get_processes_list())
+                iterator += 1
+
+        return processes
+
 
 
 # class TimeoutException(Exception):
