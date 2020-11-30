e3766953b6e15a4116fdf005ffaa4fd8fff56e15
psm8 <pioseem8@gmail.com>
2020-11-30 01:13:17 +0100

    change README

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 add info about visited nodes and no of children, could be stored in dictionary created when parsing.
 ### Generalization
 add info about visited nodes and no of children, could be stored in dictionary created when parsing.
+
+merge lop if repetitions
--- src/test/test.py
+++ src/test/test.py
@@ -1,5 +1,6 @@
 from fitness.process_fitness import evaluate_guess
 
+evaluate_guess('{f}xor({d}and({b}lop({b})opt({a})))')
 evaluate_guess('lop(opt(seq(xor({e}{c})lop({d}))){f})')
 evaluate_guess('and(lop(opt(seq(xor({e}{c})lop({d}))){f}){a})and({d}{a})')
 evaluate_guess('lop(xor({b}xor({a}{e}{d})){e})')
